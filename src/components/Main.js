c72c1d0f23c71b97224a8537343ee8be2be3aa24
Jessie Peng <jessie.jie.peng@hotmail.com>
2018-11-05 07:10:57 +1100

    clean up

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,10 @@ require('styles/App.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//获取图片相关的数据
+//require information about the photos.
 let imageData = require('../data/imageData.json');
 
-//利用函数，将图片名信息转成图片URL路径信息
+//to obtain photo URLs from filename and file path.
 function genImageURL(imageDataArr) {
   for (let i = 0, j = imageDataArr.length; i < j; i++) {
     let singleImageData = imageDataArr[i];
@@ -19,13 +19,14 @@ function genImageURL(imageDataArr) {
 
 imageData = genImageURL(imageData);
 
-//获取区间内的一个随机值
+//return a random number in the range of (low, high)
 function getRangeRandom(low, high) {
   return Math.ceil(Math.random() * (high - low) + low);
 
 }
 
 //获取0-30度之间的一个任意正负值
+//return a random degeree between 0-30 degree, it could be positive or negative
 function get30DegRandom() {
   return ((Math.random() > 0.5 ? '' : '-') + Math.ceil(Math.random() * 30));
 }
@@ -48,12 +49,11 @@ class ImgFigure extends React.Component {
 
   render() {
     let styleObj = {};
-    //如果props属性中制定了这张图片的位置，则使用
+  
     if (this.props.arrange.pos) {
       styleObj = this.props.arrange.pos;
     }
 
-    //如果图片的旋转角度有值并且不为0， 添加旋转角度
     if (this.props.arrange.rotate) {
       (['MozTransform', 'msTransform', 'WebkitTransform', 'transform']).forEach((value) => {
         styleObj[value] = 'rotate(' + this.props.arrange.rotate + 'deg)';
@@ -134,9 +134,9 @@ class AppComponent extends React.Component {
     }
   }
 
-  //翻转图片
-  //@param index 输入当前被执行inverse操作的图片index
-  //@return {Function} 这是一个必报函数，其内return一个真正待执行的函数
+  //inverse picture
+  //@param index 
+  //@return {Function} 
   inverse(index) {
     return () => {
       let imgsArrangeArr = this.state.imgsArrangeArr;
@@ -147,69 +147,7 @@ class AppComponent extends React.Component {
     }
   }
 
-  //重新布局所有图片
-  //param centerIndex制定居中排布哪个图片
-  /* rearrange(centerIndex) {
-    let imgsArrangeArr = this.state.imgsArrangeArr,
-      Constant = this.Constant,
-      centerPos = Constant.centerPos,
-      hPosRange = Constant.hPosRange,
-      vPosRange = Constant.vPosRange,
-      hPosRangeLeftSecX = hPosRange.leftSecX,
-      hPosRangeRightSecX = hPosRange.rightSecX,
-      hPosRangeY = hPosRange.y,
-      vPosRangeTopY = vPosRange.topY,
-      vPosRangeX = vPosRange.x,
-      imgsArrangeTopArr = [], //用来存储布局在上侧区域的图片状态信息
-      topImgNum = Math.floor(Math.random() * 2),  //上侧区域有0个或者1个图片
-      topImgSpliceIndex = 0,  //用来标记上侧区域的图片序号
-      imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
-
-      //首先居中centerIndex图片，centerIndex图片不需要旋转
-      imgsArrangeCenterArr[0] = centerPos;
-      //去除要布局上侧的图片的状态信息
-      topImgSpliceIndex = Math.ceil(Math.random() * imgsArrangeArr.length - topImgNum);
-      console.log('centerIndex ' + centerIndex + ' topIndex ' + topImgSpliceIndex + ' topNum ' + topImgNum);
-      imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
-      //布局位于上侧的图片
-      imgsArrangeTopArr.forEach((value, index) => {
-        imgsArrangeTopArr[index].pos = {
-          top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
-          left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
-        };
-      });
-
-      //布局左右两侧的图片
-      for (let i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
-        let hPosRangeLORX = null;
-        //前半部分布局在左边，后半部分布局在右边
-        if (i < k) {
-          hPosRangeLORX = hPosRangeLeftSecX;
-        } else {
-          hPosRangeLORX = hPosRangeRightSecX;
-        }
-        imgsArrangeArr[i].pos = {
-          top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
-          left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
-        }
-      }
-
-      //重新合并回来
-      //如果上侧区域有一个图片
-      if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
-        imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
-    }
-
-    imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
-      for ( let i = 1; i < imgsArrangeArr.length; i++) {
-        console.log( imgsArrangeArr[i].pos.top + ' ' + imgsArrangeArr[i].pos.left);
-      }
-      this.setState({
-        imgsArrangeArr: this.state.imgsArrangeArr
-      });
-  } */
-
-  //重新布局所有图片
+  //re-arrange all the photos
   rearrange(centerIndex) {
     let imgsArrangeArr = this.state.imgsArrangeArr,
       Constant = this.Constant,
@@ -222,19 +160,19 @@ class AppComponent extends React.Component {
       vPosRangeTopY = vPosRange.topY,
       vPosRangeX = vPosRange.x,
       imgsArrangTopArr = [],
-      topImgNum = Math.floor(Math.random() * 2), //取一个或者不取
+      topImgNum = Math.floor(Math.random() * 2), //at most one photo in the top area. 
       topImgSpiceIndex = 0,
       imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
-      //首先居中centerIndex图片 ,centerIndex图片不需要旋转
+  
       imgsArrangeCenterArr[0] = {
         pos: centerPos,
         rotate: 0,
         isCenter: true
       }
-      //取出要布局上测的图片的状态信息
+      
     topImgSpiceIndex = Math.floor(Math.random() * (imgsArrangeArr.length - topImgNum));
     imgsArrangTopArr = imgsArrangeArr.splice(topImgSpiceIndex, topImgNum);
-    //布局位于上侧的图片
+    //re-arrange photos in the top area
     imgsArrangTopArr.forEach((value, index) => {
       imgsArrangTopArr[index] = {
         pos: {
@@ -247,11 +185,10 @@ class AppComponent extends React.Component {
       };
     });
 
-    //布局左两侧的图片
+    //re-arrange photos in the left and right areas.
     for (let i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
       let hPosRangeLORX = null;
 
-      //前半部分布局左边,右边部分布局右边
       if (i < k) {
         hPosRangeLORX = hPosRangeLeftSecX;
       } else {
@@ -284,7 +221,7 @@ class AppComponent extends React.Component {
     }
   }
 
-  //组件加载以后为每张图片计算其位置的范围
+  //compute the new positions for all the photos.
   componentDidMount() {
     let stageDOM = ReactDOM.findDOMNode(this.refs.stage);
     let stageW = stageDOM.scrollWidth;
@@ -353,7 +290,6 @@ class AppComponent extends React.Component {
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
